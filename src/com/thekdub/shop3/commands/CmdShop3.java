c255feb88d091357f66931f532fe238da6fd5903
TheKDub <thekdub@users.noreply.github.com>
2020-08-27 20:22:53 -0400

    It helps when you include the file you're trying to commit... woops...

--- src/com/thekdub/shop3/commands/CmdShop3.java
+++ src/com/thekdub/shop3/commands/CmdShop3.java
@@ -6,6 +6,40 @@ import org.bukkit.command.CommandSender;
 
 public class CmdShop3 extends Cmd {
   public static boolean execute(CommandSender sender, Command cmd, String[] args) {
+
+    /* Command Responses...
+
+    /Shop3
+    ------
+    [Shop^3] Plugin Information:
+      Author: TheKDub
+      Version: 1.0.0
+       > Use /Shop3 Insight for information about which items to sell for maximum profits
+       > Use /Shop3 History for a list of your Shop3 transaction history
+      For a list of additional commands, use /Shop3 Help
+
+    /Shop3 Help
+    ------
+    [Shop3] Commands:
+     > /Buy - Purchase items from Shop^3
+     > /CancelSell - Cancel selling items
+     > /QuickSell - Quickly sell items directly to Shop^3
+     > /Sell - Place items up for sale
+     > /Selling - View a list of items that you're selling
+     > /Shop3 - Access plugin information and commands
+     > /Stock - View an item's stock information
+     Use /Shop3 Help (Command) to view more information about a command.
+
+    /Shop3 Help Buy | /Shop3 Help /Buy (Strip input)
+    [Shop3] /Buy Command:
+      Description: Purchase items from Shop^3
+      Usage: /Buy (item name or id[:data]) [amount]
+
+     /Shop3 Help CancelSell
+
+    */
+
+
     return false;
   }
 }
