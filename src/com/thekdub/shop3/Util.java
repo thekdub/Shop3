40e8e11bb50fd7aa7311d83c3c083eb27e9453cf
TheKDub <thekdub@users.noreply.github.com>
2020-09-01 19:51:59 -0400

    More javadoc comments, kinda... I'm slacking ;-;

--- src/com/thekdub/shop3/Util.java
+++ src/com/thekdub/shop3/Util.java
@@ -4,19 +4,36 @@ import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 
 public class Util {
-  
+
+  /**
+   * @param itemStack An ItemStack object.
+   * @return The name of the ItemStack.
+   */
   public static String getItemName(ItemStack itemStack) {
     return getItemName(itemStack.getTypeId(), itemStack.getDurability());
   }
-  
+
+  /**
+   * @param id An item ID.
+   * @return The name of the item ID.
+   */
   public static String getItemName(int id) {
     return getItemName(id, (short) 0);
   }
-  
+
+  /**
+   * @param id An item ID.
+   * @param durability An item durability value.
+   * @return The name of the item ID and durability value.
+   */
   public static String getItemName(int id, short durability) {
     return id + ":" + durability;
   }
-  
+
+  /**
+   * @param s The string you want to translate color codes in.
+   * @return A color code translated string.
+   */
   public static String translateColor(String s) {
     return ChatColor.translateAlternateColorCodes('&', s);
   }
